697bfe01e0b3f79c3ec92ebbbe356e9faa029bab
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-05-10 11:33:45 +0300

    Check tests

--- spec/dummy_app/config/boot.rb
+++ spec/dummy_app/config/boot.rb
@@ -1,4 +1,11 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+class File
+  def self.exists?(file_name)
+    exist? file_name
+  end
+end
+
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
